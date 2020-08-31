1ee84dd7d7d0efb47c3e442b52528721b07994e5
Elmatador <kmouad29@gmail.com>
2020-08-31 11:43:35 -0600

    push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Header from "./compenents/layout/Header";
 import Todos from "./compenents/Todos";
 import AddTodo from "./compenents/AddTodo";
@@ -58,19 +58,16 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div className="App">
-          <div className="container">
-            <Header />
-            <AddTodo AddTodo={this.AddTodo} />
-            <Todos
-              todos={this.state.todos}
-              markComplete={this.markComplete}
-              markDelete={this.markDelete}
-            />
-          </div>
+      <div className="App">
+        <div className="container">
+          <Header /> <AddTodo AddTodo={this.AddTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            markDelete={this.markDelete}
+          />
         </div>
-      </Router>
+      </div>
     );
   }
 }
